c729e4df59c6f5d15167bf536abb4cf7192dd9ef
Zach <a10135198@gmail.com>
2019-07-09 16:26:04 +0800

    上傳作業
    HomeWork_Day1

---
+++ Account/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Account.Models;
+
+
+
+namespace Account.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+       
+        public ActionResult Data()
+        {
+            List<ViewModels> display = new List<ViewModels>();
+
+            Random rdm = new Random();
+            for (int i = 1; i <= 100; i++)
+            {
+
+                int randomnumber = rdm.Next(101);
+                int randomtype = rdm.Next(1, 3);
+                var randomdatetime = DateTime.Now.AddDays(randomnumber);
+                int randommoney = rdm.Next(500, 3001);
+                display.Add(new ViewModels 
+                {
+                    Type = (randomtype == 1) ? "收入" : "支出",
+                    DateTime = randomdatetime,
+                    Money = randommoney,
+                });
+            }
+            return View(display);
+        }
+
+       
+      
+
+    }
+}
---
+++ Account/Models/ViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Account.Models
+{
+    public class ViewModels
+    {
+       
+        public string Type { get; set; }
+
+
+        [RegularExpression(@"^[0-9]+$")]
+        public int Money { get; set; }
+
+        [Required(ErrorMessage ="請輸入正確時間")]
+        public DateTime DateTime { get; set; }
+
+        public string Remark { get; set; }
+
+
+    }
+}
